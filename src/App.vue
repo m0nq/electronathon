54ceddfa6c1e8394a4c679f949b98e0006b5c502
Monk Wellington <forgetmenaught@gmail.com>
2019-08-14 10:26:30 -0700

    Modal displayed with bootstrap animations

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
       :cartQty="cartQty"
       :cartTotal="cartTotal"
       :products="products"
+      :modalPhoto="modalPhoto"
       @add="addItem"
       @delete="deleteItem"
       @setCurrentPhoto="setCurrentPhoto"
